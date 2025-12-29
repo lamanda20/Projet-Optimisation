af692a8ff136c5a9d4633670ac63ed186558fd74
Anass Chadli <anasschadli.me@gmail.com>
2025-12-29 09:18:28 +0100

    refactored the structure and removed duplicates

--- utils/distance.py
+++
@@ -1,8 +0,0 @@
-import math
-from typing import List, Tuple, Dict
-
-def distance_grille(p1: Tuple[int, int], p2: Tuple[int, int]) -> float:
-    """Distance euclidienne sur grille discrète"""
-    dx = p1[0] - p2[0]
-    dy = p1[1] - p2[1]
-    return math.sqrt(dx**2 + dy**2)
